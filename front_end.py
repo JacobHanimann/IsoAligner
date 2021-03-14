e3d64de83892f7c02fa85337152bc2df76a77733
JacobHanimann <jacob.hanimann@gmail.com>
2021-03-14 13:46:29 +0100

    front end design

--- front_end.py
+++ front_end.py
@@ -41,7 +41,7 @@ def main():
         with header:
             # Title
             st.title(" Amino Acid Isoform Aligner")
-            st.subheader("Map amino acid positions across isoforms")
+            st.subheader("Map Amino Acid Positions Across Isoforms")
             st.write(
                 "Align isoforms with the Needleman-Wunsch algorithm and set the minimal exon length to discard falsely mapped positions (random matches) of two distinct exons."
                 " The table of correctly mapped positions can be downloaded as a file in several formats. A preview of the alignments is displayed dynamically. ")
