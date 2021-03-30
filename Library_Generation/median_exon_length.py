414a3b1c1a5bc7180ac8a92ac16857d5420b6811
JacobHanimann <jacob.hanimann@gmail.com>
2021-03-30 16:21:31 +0200

    adding exon information

--- Library_Generation/median_exon_length.py
+++ Library_Generation/median_exon_length.py
@@ -4,12 +4,13 @@ from Gene import *
 from Protein_isoform import *
 from Alignment import *
 from Extractions_BioIDs import *
+from Exon import *
 
 class Exon_Information():
     pass
 
     @staticmethod
-    def read_Ensembl_GRCh38_gtf_file(file):
+    def read_Ensembl_GRCh38_gtf_file_generate_nested_dict(file):
         '''extract fasta files one by one and add them to the gene objects'''
         with open(file, "r") as f:
             expenses_txt = f.readlines()
@@ -24,7 +25,27 @@ class Exon_Information():
             fasta_count += 1
 
 
+    @staticmethod
+    def pick_exon_length_median_from_nested_dict():
+        pass
+
+
+
+def add_exon_median_to_gene_objects():
+    pass
+
+def add_exon_objects_to_protein_objects():
+    pass
+
+
 #Execution
 
 Exon_Information.read_Ensembl_GRCh38_gtf_file('/Users/jacob/Desktop/Isoform Mapper Webtool/Homo_sapiens.GRCh38_protein_coding.gtf')
 
+#nested dict as an output
+
+#newdict with median of each ENSG
+
+#assign new dict to gene object
+
+#assign exon objects to protein isoforms if wanted
