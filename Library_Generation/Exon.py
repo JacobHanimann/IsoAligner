5f8e9d2909c6c00eb0e53c2de8b0c8ee762fc46b
JacobHanimann <jacob.hanimann@gmail.com>
2021-03-30 15:45:24 +0200

    adding exon information

---
+++ Library_Generation/Exon.py
@@ -0,0 +1,12 @@
+
+class Exon():
+    def __init__(self,):
+        self.exon_start_end
+        self.exon_length
+        self.exon_ID
+        self.transcript_ID
+        self.exon_number
+
+
+#exons object definiert durch ENSG und ENST nummern, exon objects kreieren und dann median ausrechenen für jedes Gen und dann exon objects zu den protein isoformen hinzufügen
+
--- Library_Generation/median_exon_length.py
+++ Library_Generation/median_exon_length.py
@@ -4,8 +4,6 @@ from Gene import *
 from Protein_isoform import *
 from Alignment import *
 from Extractions_BioIDs import *
-import gzip
-
 
 class Exon_Information():
     pass
@@ -13,8 +11,10 @@ class Exon_Information():
     @staticmethod
     def read_Ensembl_GRCh38_gtf_file(file):
         '''extract fasta files one by one and add them to the gene objects'''
-        with gzip.open(file, "r") as f:
+        with open(file, "r") as f:
             expenses_txt = f.readlines()
+        for line in expenses_txt:
+            print(line)
         whole_txt = "".join(expenses_txt)
         splittext = re.split(">", whole_txt)
         fasta_count = 0
@@ -26,5 +26,5 @@ class Exon_Information():
 
 #Execution
 
-
+Exon_Information.read_Ensembl_GRCh38_gtf_file('/Users/jacob/Desktop/Isoform Mapper Webtool/Homo_sapiens.GRCh38_protein_coding.gtf')
 
