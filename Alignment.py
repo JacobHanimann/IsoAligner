68d21c349a037ea393009e5f5e58755e2089398a
JacobHanimann <jacob.hanimann@gmail.com>
2021-05-27 16:28:36 +0200

    created mismatch category

--- Alignment.py
+++ Alignment.py
@@ -45,7 +45,7 @@ class Alignment:
                     gap = True
                 else:
                     category='mismatch'
-                    st.write('Mismatch detected')
+                    st.error('mismatch detected: displayed as big letter X instead of x')
                     isoform_check.append(category)
                     continue
             else:  # same Aminoacid
